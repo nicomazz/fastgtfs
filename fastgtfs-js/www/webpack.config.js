ffc3b355ca4636d074eb42d2ff316fb639dd218d
gamma <gammabetaalpha@protonmail.com>
2021-07-25 16:42:38 +0200

    Add markers

--- fastgtfs-js/www/webpack.config.js
+++ fastgtfs-js/www/webpack.config.js
@@ -2,15 +2,21 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: ["./bootstrap.js"],
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
-  },
-  mode: 'production',
-  plugins: [
-    new CopyWebpackPlugin(['index.html']),
-    new CopyWebpackPlugin(['gtfs_serialized.zip']),
-    new CopyWebpackPlugin(['index.js']),
-  ],
+    entry: ["./bootstrap.js"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bootstrap.js",
+    },
+    experiments: {asyncWebAssembly: true},
+    mode: 'production',
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: 'index.html', to: 'dist/index.html'},
+                {from: 'gtfs_serialized.zip', to: 'dist/gtfs_serialized.zip'},
+                {from: 'index.js', to: 'dist/index.js'},
+
+            ]
+        })
+    ],
 };
